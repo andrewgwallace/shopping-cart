37943904bcbdd81a3fd76a5756779bc1723b6186
andrewgwallace <andrew.g.wallace@gmail.com>
2018-05-10 17:15:11 -0400

    Complete exercise one lesson 67

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import CartHeader from './CartHeader';
+import CartFooter from './CartFooter';
+import CartItems from './CartItems';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CartHeader />
+        <CartItems />
+        <CartFooter />
+      </div>
+    );
+  }
+}
+
+export default App;
