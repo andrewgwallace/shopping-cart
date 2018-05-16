80ebf39a4b2f68dd594b5704748f3a6125fdcdcd
andrewgwallace <andrew.g.wallace@gmail.com>
2018-05-16 17:01:57 -0400

    List shopping cart items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import CartHeader from './CartHeader';
-import CartFooter from './CartFooter';
-import CartItems from './CartItems';
+import CartHeader from './components/CartHeader';
+import CartFooter from './components/CartFooter';
+import CartItems from './components/CartItems';
 
 class App extends Component {
   render() {
+    let cartItemsList = [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+    ]
     return (
       <div className="App">
         <CartHeader />
-        <CartItems />
+        <CartItems stuff = {cartItemsList} />
         <CartFooter />
       </div>
     );
---
+++ src/components/CartItems.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+
+
+const CartItems = ({stuff}) => {
+  return (
+    <div className="container">
+      <h1>Cart Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
+          </div>
+        </div>
+        {stuff.map(item => <CartItem key={item.id} item={ item } />)}
+      </div>
+    </div>
+  )
+}
+
+export default CartItems;
